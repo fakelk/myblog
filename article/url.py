e38e61555a4804a6dd3fe49cf81fbc72d389f930
fakelk <lukmankh79@yahoo.com>
2017-08-25 09:45:30 +0700

    anu

--- article/url.py
+++ article/url.py
@@ -5,4 +5,5 @@ url patterns = [
     url(r'^myview/$',views.myview),
     url(r'^hello world/$',views.hello_world),
     url(r'^nama/p<nama>[\w-]+)/(?p<umur>[0-9]+)$',nama_saya, umur),
+     url(r'^(?p<slug>[0-9]+)$',views.post_detail),
 ]
--- myblog/urls.py
+++ myblog/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin  
+from article.views import home
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^tinymce/', include('tinymce.urls')),
+    url(r'^tinymce/', ('tinymce.urls')),
+]
